f0c2fa5e433ae69c1bb2808f60348a6d5a961dc0
AdemFat <medk57597@gmail.com>
2020-06-09 17:39:47 +0100

    initial
    setting project

---
+++ Pong Game/Assets/Scenes/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool isPlayer1;
+    public float speed;
+    public Rigidbody2D rb;
+
+    private float movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}    
+  
