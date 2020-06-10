8b962166d28cb7043630c70516b4085e41a0fe5b
AdemFat <medk57597@gmail.com>
2020-06-10 16:10:39 +0100

    CP2
    functions

---
+++ Pong Game/Assets/Scenes/Cp2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cp2 : MonoBehaviour
+{
+    public int a, b;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Sum(a, b));
+        Debug.Log(Prod(a, b));
+        Debug.Log(Div(a, b));
+    }
+
+    private int Sum(int x, int y)
+    {
+        return x + y;
+    }
+
+    private int Prod(int x, int y)
+    {
+        return x * y;
+    }
+
+
+    private int Div(int c, int d)
+    {
+        if (b == 0)
+        {
+            Debug.Log("You can't devide by 0");
+            return 0;
+        }
+        else
+        {
+            return c / d;
+        }
+    }
+}
